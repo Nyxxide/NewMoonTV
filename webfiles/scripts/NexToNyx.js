3fb1637dc143dc58cece09b7c2dbb8cc7ac7c366
Matthew Fierek <mfierek@cs.stonybrook.edu>
2024-05-15 02:32:53 -0400

    Made things less gross

---
+++ webfiles/scripts/NexToNyx.js
@@ -0,0 +1,6 @@
+var switchText = document.getElementById("nexus");
+
+setTimeout(function () {
+    switchText.innerText = "NYXXUS";
+    switchText.style.transform = "scaleX(1.6)";
+}, 2000);
--- webfiles/scripts/resize.js
+++ webfiles/scripts/resize.js
@@ -1,1 +1,7 @@
-//Ill come back here later
+//(desired resolution) = 20vh(e)^-0.00028863(monitor resolution - window size)
+while(true){
+    let windowWidth = window.innerWidth;
+    let windowHeight = window.innerHeight;
+    let screenWidth = screen.width;
+    let screenHeight = screen.height;
+}
