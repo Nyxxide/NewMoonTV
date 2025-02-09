5d81e6b54428441e5f08d05068f72bbae535e058
Matthew Fierek <mfierek@cs.stonybrook.edu>
2025-02-09 04:12:11 -0500

    Seeeecret Debug Switch

--- webfiles/scripts/SiteHandler.js
+++ webfiles/scripts/SiteHandler.js
@@ -1,6 +1,7 @@
 //TODO: Add stars in foreground
 
 var width = window.innerWidth;
+var pageState = "default";
 var keepLooping = true;
 var fonts = ["\"Jersey 25\"", "Akaya Kanadaka", "Architects Daughter", "Atomic Age", "Bagel Fat One", "Barrio",
                     "Black Ops One", "Calligraffitti", "Trade Winds", "Finger Paint", "CURSEOFRA", "Creepster", "Cute Font",
@@ -24,6 +25,7 @@ var banner = document.getElementById("head");
 var buttonbody = document.getElementById("grid");
 var buttons = document.getElementsByClassName("linkbutton");
 var backbutton = document.getElementById("back");
+var settingsbutton = document.getElementById("settings");
 var icon = document.getElementById("favicon");
 var imgleft = document.getElementById("imgleft");
 var imgright = document.getElementById("imgright");
@@ -91,6 +93,76 @@ function moonHoverEnd() {
     glitchlayer3.classList.remove("glitch__layer");
 }
 
+async function changeTheme(){
+    if(pageState==="default"){
+        nexus.style.transition = "none";
+        nexus.style.color = "#11b7ff";
+        nexus.innerText = "NYXXUS";
+        await sleep (100);
+        resetPage();
+        nexus.style.fontFamily = "\"Jersey 25\"";
+        backbutton.style.opacity = "1";
+        settingsbutton.style.opacity = "0";
+        title.classList.add("lighting");
+        keepLooping = true;
+        let index = 0;
+        while (keepLooping) {
+            if (!keepLooping || pageState !== "default") {
+                break;
+            }
+            nexus.style.color = "#ff27d4";
+            let tmp = Math.floor(Math.random() * (fonts.length));
+            while (tmp === index) {
+                tmp = Math.floor(Math.random() * (fonts.length));
+            }
+            index = tmp;
+            await sleep(1250);
+            nexus.style.color = "#11fcff";
+            if (fonts[index] === "CURSEOFRA") {
+                nexus.innerText = "𓆣𓋹𓂀𓂀𓁨𓅓";
+                if (width > 1400) {
+                    nexus.style.fontSize = "3vw";
+                } else if (width <= 1400 && width >= 600) {
+                    nexus.style.fontSize = "42px";
+                } else {
+                    nexus.style.fontSize = "8vw"
+                }
+                nexus.style.cursor = "pointer";
+                nexus.addEventListener("click", curseofraFunc);
+            } else {
+                nexus.removeEventListener("click", curseofraFunc);
+                nexus.innerText = "NYXXUS"
+                nexus.style.cursor = "default";
+                nexus.style.fontFamily = fonts[index];
+                if (index === 0) {
+                    if (width > 1400) {
+                        nexus.style.fontSize = "5vw";
+                    } else if (width <= 1400 && width >= 600) {
+                        nexus.style.fontSize = "70vpx";
+                    } else {
+                        nexus.style.fontSize = "12vw"
+                    }
+                } else {
+                    if (width > 1400) {
+                        nexus.style.fontSize = "4vw";
+                    } else if (width <= 1400 && width >= 600) {
+                        nexus.style.fontSize = "56px";
+                    } else {
+                        nexus.style.fontSize = "10vw"
+                    }
+                }
+            }
+            nexus.classList.add("nexus-glitch");
+            await sleep(500);
+            nexus.classList.remove("nexus-glitch");
+            await sleep(3000);
+        }
+    }
+    else if(pageState==="curseofra"){
+        curseofraFunc();
+    }
+}
+
 
 async function backAnim(){
     backbutton.src = "/images/BackStep2.png";
@@ -112,6 +184,8 @@ function sleep(ms) {
 
 function resetPage() {
 
+    pageState = "default";
+
     let curseofra = document.getElementById("curseofra");
 
     htmlbody.style.backgroundImage = "none";
@@ -120,13 +194,20 @@ function resetPage() {
     banner.style.borderBottom = "2px solid #323c69";
 
     nexus.style.border = "none";
-    nexus.style.color = "#11b7ff";
-    nexus.innerText = "NEXUS";
     nexus.style.borderRadius = "0px"
     nexus.removeEventListener("click", curseofraFunc);
     nexus.style.backgroundImage = "none";
     nexus.style.transition = "opacity 1.25s 0.6s ease-in, color 1.25s 375ms linear";
     nexus.style.fontFamily = "\"Jersey 25\""
+    if(width > 1400) {
+        nexus.style.fontSize = "5vw";
+    }
+    else if(width <= 1400 && width >= 600){
+        nexus.style.fontSize = "70px";
+    }
+    else{
+        nexus.style.fontSize = "10vw";
+    }
 
     title.innerText = "Welcome to the";
     title.style.color = "#11b7ff";
@@ -174,9 +255,14 @@ function resetPage() {
     backbutton.addEventListener("mouseenter", backAnim);
     backbutton.addEventListener("mouseleave", undoBackAnim);
 
+    settingsbutton.style.opacity = "0";
+    settingsbutton.src = "/images/wall.png";
+    settingsbutton.style.border = "none";
+    settingsbutton.style.borderRadius = "0px";
+
     keepLooping = false;
 
-    icon.href = "/images/Moon.ico";
+    icon.href = "/images/Nyxxus.ico";
 
 
 
@@ -189,6 +275,8 @@ function resetPage() {
 
 function curseofraFunc(event) {
     keepLooping = false;
+
+    pageState = "curseofra";
 
     let curseofra = document.getElementById("curseofra");
 
@@ -234,7 +322,9 @@ function curseofraFunc(event) {
     nexus.style.color = "black";
     nexus.style.fontWeight = "bold";
     nexus.style.cursor = "default";
-    event.target.removeEventListener("click", curseofraFunc);
+    if(event){
+        event.target.removeEventListener("click", curseofraFunc);
+    }
     nexus.style.backgroundImage = "url(\'/images/sand.png\')";
     nexus.style.border = "black 2px solid";
     nexus.style.borderRadius = "20px";
@@ -289,13 +379,16 @@ function curseofraFunc(event) {
     icon.href = "/images/Eye.ico";
 
     backbutton.src = "/images/Man.png"
-    backbutton.style.width = "50px";
     backbutton.style.border = "solid 4px black";
     backbutton.style.borderRadius = "10px";
     backbutton.removeEventListener("mouseenter", backAnim);
     backbutton.removeEventListener("mouseleave", undoBackAnim);
     backbutton.addEventListener("mouseenter", backHover);
     backbutton.addEventListener("mouseleave", backStopHover);
+
+    settingsbutton.src = "/images/slope.png"
+    settingsbutton.style.border = "solid 4px black";
+    settingsbutton.style.borderRadius = "10px";
 
     for(let i = 0; i < buttons.length; i++){
         buttons[i].style.backgroundImage = "url(\'/images/sand.png\')";
@@ -380,6 +473,8 @@ window.addEventListener("resize", () => {
 });
 glitchdiv.addEventListener("click", async (event) => {
     // resetPage();
+    nexus.style.color = "#11b7ff";
+    nexus.innerText = "NEXUS";
     keepLooping = true;
     if(width > 1400){
         nexus.style.fontSize = "5vw";
@@ -420,16 +515,18 @@ glitchdiv.addEventListener("click", async (event) => {
     nexus.classList.add("nexus-glitch");
     nexus.innerText = "NYXXUS";
     backbutton.style.display = "block";
+    settingsbutton.style.display = "block";
     await sleep(500);
     backbutton.style.opacity = "1";
     backbutton.addEventListener("mouseenter", backAnim);
     backbutton.addEventListener("mouseleave", undoBackAnim);
+    settingsbutton.style.opacity = "0";
     title.classList.add("lighting");
     nexus.classList.remove("nexus-glitch");
     let index = 0;
     await sleep(3000);
     while(keepLooping){
-        if(!keepLooping){
+        if(!keepLooping || pageState !== "default"){
             break;
         }
         nexus.style.color= "#ff27d4";
@@ -489,7 +586,7 @@ glitchdiv.addEventListener("click", async (event) => {
     }
 });
 
-backbutton.addEventListener("click", async (event) => {
+backbutton.addEventListener("click", async () => {
     background.style.display = "block";
     background.style.opacity = "1";
     foreground.style.animation = "fadein 1s reverse";
@@ -503,6 +600,16 @@ backbutton.addEventListener("click", async (event) => {
     moon.style.animation = "imgGlitch 5.0s infinite, pulse2 5.0s infinite";
     glitchdiv.addEventListener("mouseenter", moonHoverStart);
     glitchdiv.addEventListener("mouseleave", moonHoverEnd);
+})
+
+settingsbutton.addEventListener("click", () => {
+    if(pageState==="default"){
+        pageState = "curseofra";
+    }
+    else if(pageState==="curseofra"){
+        pageState = "default";
+    }
+    changeTheme();
 })
 
 glitchdiv.addEventListener("mouseenter", moonHoverStart);
